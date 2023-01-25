54ba9dc54fac09d5e286bb61d932669f8e108224
WhiteBorshek <121310443+WhiteBorshek@users.noreply.github.com>
2023-01-25 14:29:28 +0500

    Add files via upload

---
+++ Homework IX.cs
@@ -0,0 +1,12 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] arr = new int[8];
+Random rand = new Random();
+for (int i = 0; i < arr.Length; i++)
+   arr[i] = rand.Next(0,50);
+
+Console.WriteLine(string.Join(", ", arr));
